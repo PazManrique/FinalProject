e5959ce51f5590053b71e63c6f940533a917e32d
Manrique <maria-paz.manrique@capgemini.com>
2022-12-19 09:00:22 +0100

    encrypting password

--- src/main/java/es/com/capgemini/finalproject/RockPaperScissorsApplication.java
+++ src/main/java/es/com/capgemini/finalproject/RockPaperScissorsApplication.java
@@ -3,7 +3,10 @@ package es.com.capgemini.finalproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class RockPaperScissorsApplication {
 
 	public static void main(String[] args) {
