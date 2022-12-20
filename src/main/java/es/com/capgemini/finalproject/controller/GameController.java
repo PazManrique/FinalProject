33aa0f7ff80bfc89030846e6857dbdcdfefabc45
Manrique <maria-paz.manrique@capgemini.com>
2022-12-20 08:51:56 +0100

    adding view controller

--- src/main/java/es/com/capgemini/finalproject/controller/GameController.java
+++ src/main/java/es/com/capgemini/finalproject/controller/GameController.java
@@ -5,8 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +22,10 @@ import es.com.capgemini.finalproject.service.ResultService;
 public class GameController  {
 	@Autowired
 	ResultService resultService;
+	
+	
 
-	 @GetMapping("/")
+	 @GetMapping("/all")
 	  public ResponseEntity<Iterable<Result>> resultList(){
 		 Iterable<Result> results = resultService.resultList();
 		 return new ResponseEntity<Iterable<Result>>(results, HttpStatus.OK);
@@ -28,7 +33,7 @@ public class GameController  {
 	 
 	 
 	 
-	 @PostMapping("/results") public Result addResult(@RequestBody Result
+	 @PostMapping("/Dresults") public Result addResult(@RequestBody Result
 			  theResult) {
 			  
 			  
@@ -38,15 +43,43 @@ public class GameController  {
 			  
 			  return theResult; }
 	 
-	 @PostMapping("/result")
-	  public String gameResult(Model model) {
-	   
-	    return "result";
-	  }
+	
 	 
-	 @GetMapping("/table")
+	 @GetMapping("/Dtable")
 	  public String gameTable(Model model) {
 	   
 	    return "table";
 	  }
+	 
+	 
+	 @PutMapping("/Dtable")
+		public Result updateProduct(@RequestBody Result theResult) {
+			
+		 resultService.saveResult(theResult);
+			
+			return theResult;
+		}
+		
+		// add mapping for DELETE /Products/{productId} - delete Product
+		
+		@DeleteMapping("/DProducts/{productId}")
+		public String deleteProduct(@PathVariable int idResults) {
+			
+			Result tempProduct = resultService.findById(idResults);
+			
+			// throw exception if null
+			
+			if (tempProduct == null) {
+				throw new RuntimeException("Product id not found - " + idResults);
+			}
+			
+			resultService.deleteById(idResults);
+			
+			return "Deleted Product id - " + idResults;
+		}
+	 
+	 
+	 
+	 
+	 
 }
