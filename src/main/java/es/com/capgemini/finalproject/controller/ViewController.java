14f56196b046a2066b8a1fb2d75322f8294ca350
María Paz Manrique <maria-paz.manrique@capgemini.com>
2022-12-22 12:12:22 +0100

    readme

--- src/main/java/es/com/capgemini/finalproject/controller/ViewController.java
+++ src/main/java/es/com/capgemini/finalproject/controller/ViewController.java
@@ -1,14 +1,15 @@
 package es.com.capgemini.finalproject.controller;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import es.com.capgemini.finalproject.model.Move;
 //@Controller
 //@RequestMapping("/game")
+
 public class ViewController {
 	
 	 @GetMapping("/")
