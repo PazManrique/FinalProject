c7b97eb21d137999775238d2b4059f0bacb6fa49
Cristina Sanchez <c.sanchez.cadrecha@gmail.com>
2022-12-22 13:10:10 +0100

    added JavaDoc Move and Result models

--- src/main/java/es/com/capgemini/finalproject/model/Move.java
+++ src/main/java/es/com/capgemini/finalproject/model/Move.java
@@ -1,10 +1,8 @@
 package es.com.capgemini.finalproject.model;
 
-
 import java.sql.Timestamp;
 
 import java.text.SimpleDateFormat;
-
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -20,36 +18,35 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Entity
-@Table(name="moves")
+@Table(name = "moves")
 public class Move {
-	
+
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idResults;
-	
-	@Column(name="player_Name",  nullable = true)
+
+	@Column(name = "player_Name", nullable = true)
 	private String playerName;
-	
-    
-    @Column(name="player_Choice")
+
+	@Column(name = "player_Choice")
 	private Integer playerChoice;
-    
-    @Column(name="computer_Choice")
+
+	@Column(name = "computer_Choice")
 	private Integer computerChoice;
 
-    
-    @Column(name = "date_Time")
-    @CreationTimestamp
-    private Timestamp dateTime;
+	@Column(name = "date_Time")
+	@CreationTimestamp
+	private Timestamp dateTime;
 
-
+	/**
+	 * Constructors
+	 */
 
 	public Move(Integer idResults, String playerName, Integer playerChoice, Integer computerChoice) {
 		super();
@@ -58,25 +55,22 @@ public class Move {
 		this.playerChoice = playerChoice;
 		this.computerChoice = computerChoice;
 	}
+	
+	 /**
+     * Method that collects the necessary data to display the result
+     */
 
-    
-  
-    
-  public Result moveResult( ) {
-	  	CharacterFactory computerChoiceNumber = CharacterFactory.getInstance(computerChoice);
+	public Result moveResult() {
+		CharacterFactory computerChoiceNumber = CharacterFactory.getInstance(computerChoice);
 		String computerChoiceName = computerChoiceNumber.getName();
 		CharacterFactory playerChoiceNumber = CharacterFactory.getInstance(playerChoice);
 		String playerChoiceName = playerChoiceNumber.getName();
-		 
-	  computerChoiceNumber.compare(playerChoiceNumber);
-	  SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
-	 
-	  return new Result(playerName, playerChoiceName, computerChoiceName, computerChoiceNumber.getDescriptionResult(), sdf.format(dateTime));
-		 }
-  
+
+		computerChoiceNumber.compare(playerChoiceNumber);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+
+		return new Result(playerName, playerChoiceName, computerChoiceName, computerChoiceNumber.getDescriptionResult(),
+				sdf.format(dateTime));
+	}
 
 }
-
-
-
-
